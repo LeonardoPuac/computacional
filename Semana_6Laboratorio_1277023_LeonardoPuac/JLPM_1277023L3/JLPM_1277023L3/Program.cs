3f02606b454456e91d91dc93150afda0a87bbd22
LeonardoPuac <142248689+LeonardoPuac@users.noreply.github.com>
2023-09-18 09:00:11 -0600

    Add files via upload

---
+++ Semana_6Laboratorio_1277023_LeonardoPuac/JLPM_1277023L3/JLPM_1277023L3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JLPM_1277023L3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ Semana_6Laboratorio_1277023_LeonardoPuac/L2_JLPM1277023/L2_JLPM1277023/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace L2_JLPM1277023;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 2");
+        Console.ReadKey();
+
+        Console.WriteLine("Ingrese número de día");
+        int número; //variable tipo entero
+        string entrada; // variable tipo cadena de texto
+
+        Console.WriteLine("Ingrese número de dia");
+        entrada = Console.ReadLine();
+        numero = Int32Parse(entrada);
+
+        Console.WriteLine(el numero es:"") + entrada
+        Console.WriteLine (El numero de es:" + numero);
+
+        if (numero == )
+        {
+            Console.WriteLine("El número a ingresar debe estar contenido entre 1 y 7");
+        }
+
+
+    }
+
+}
+

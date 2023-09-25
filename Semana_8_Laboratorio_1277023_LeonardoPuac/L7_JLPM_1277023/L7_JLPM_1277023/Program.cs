997d9a226ae70966619584dff6882cd0afb10565
LeonardoPuac <142248689+LeonardoPuac@users.noreply.github.com>
2023-09-25 09:59:11 -0600

    Add files via upload

---
+++ Semana_8_Laboratorio_1277023_LeonardoPuac/L7_JLPM_1277023/L7_JLPM_1277023/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace L7_JLPM_1277023;
+
+using System;
+
+internal class Program
+{
+
+    static void Main(string[]args)
+    {
+        Console.WriteLine("\nIngrese un número");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int A = 0, B = 1, C = 0, i = 2;
+        string resultados = "";
+        resultados = A.ToString();
+
+        if (N > 0)
+        {
+            resultados = A.ToString();
+
+            if (N > 1)
+            {
+                resultados = B.ToString();
+
+                while (N > i)
+                {
+                    C = A + B;
+                    resultados = C.ToString();
+                    A = B;
+                    B = C;
+                    i++;
+
+                }
+                Console.WriteLine("Resultados " + resultados);
+            }
+        
+           else
+          {
+            Console.WriteLine("Reslutados " + resultados);
+          }
+        }
+        else
+        {
+            Console.WriteLine("Resultados " + resultados);
+        }
+        Console.ReadKey();
+
+
+
+    }
+
+}

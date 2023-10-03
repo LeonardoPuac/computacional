87e0260809896c18376d948743e956d4ea3d8332
LeonardoPuac <142248689+LeonardoPuac@users.noreply.github.com>
2023-10-02 22:18:08 -0600

    Add files via upload

---
+++ Semana_9_Labooratorio_1277023_LeonardoPuac/L9_JLPM_1277023/L9_JLPM_1277023/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace L9_JLPM_1277023;
+
+class Program 
+
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Calcula la sumatoria desde 1 hasta el número ingresado usando un do-while
+            int sumatoria = 0;
+            int contador = 1;
+            do
+            {
+                sumatoria += contador;
+                contador++;
+            } while (contador <= numero);
+
+            Console.WriteLine("Resultado de sumatoria " + sumatoria);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("\nTabla del " + i + ":\t");
+
+                for (int j = 1; j <= numero; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
+

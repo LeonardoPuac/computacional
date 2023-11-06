76d38c98dae44a4df1f309e91b7b97df31776806
LeonardoPuac <142248689+LeonardoPuac@users.noreply.github.com>
2023-11-06 09:11:00 -0600

    Add files via upload

---
+++ SEMANA_14_LABORATORIO_1277023_LEONARDO PUAC/L14_JLPM_1277023/L14_JLPM_1277023/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace L14_JLPM_1277023;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ingrese un número entero");
+        Console.ReadLine("");
+
+        if (" número es entero es correctop ")
+            Console.ReadLine("número " + " "1, 2, 3, 4, 5");
+        else (" nu4meor no es entero es invalido ");
+
+    }
+    Console.string()
+    {
+}
+
